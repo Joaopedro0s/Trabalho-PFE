cd767c4640a2625111d1a00f79de53d0ed88f9c5
Aluno <jeipi7007@gmail.com>
2025-05-20 09:05:58 -0300

    Primeira versão

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
-document.getElementById("meu-botao")
+document.getElementById("butao")
 .addEventListener("click", function(){
     alert("Clicado");
 })
 
-document.getElementById("meu-botao2")
+document.getElementById("double")
 .addEventListener("dblclick", function(){
     alert("Clicado");
 })
+
+document.getElementById("passar")
+.addEventListener("mouseover", function(){
+    alert("Clicado");
+})
+
+document.getElementById("sair")
+.addEventListener("mouseout", function(){
+    alert("Clicado");
+})
+
+// Keydown - Pressionando qualquer tecla
+document.addEventListener("keydown", () => {
+    const btn = document.getElementById("keydown");
+    btn.classList.add("pressionado");
+});
+
+// Keyup - Soltando a tecla
+document.addEventListener("keyup", () => {
+    const btn = document.getElementById("keyup");
+    btn.classList.add("solto");
+
+    // Remover o efeito após um tempo
+    setTimeout(() => {
+        btn.classList.remove("solto");
+        document.getElementById("keydown").classList.remove("pressionado");
+    }, 500);
+});
