553c4bbb7e4c2f50828579cc3f3af994780ee04c
Aluno <jeipi7007@gmail.com>
2025-05-20 08:16:23 -0300

    Primeira versão

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("meu-botao")
+.addEventListener("click", function(){
+    alert("Clicado");
+})
+
+document.getElementById("meu-botao2")
+.addEventListener("dblclick", function(){
+    alert("Clicado");
+})
